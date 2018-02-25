de21816b6c062b5b478d9c20c759a4bc87df083e
ThomasGiulianelli <giuliat3@tcnj.edu>
2018-02-25 00:41:50 -0500

    run.py now responds with Calorie info

--- run.py
+++ run.py
@@ -8,11 +8,20 @@ app = Flask(__name__)
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
+    # Get message sent by the user
+    body = request.values.get('Body', None)
+    
     # Start our response
     resp = MessagingResponse()
 
-    # Add a message
-    resp.message("Ahoy! Thanks so much for your message.")
+    # Ensure correct input (digits only)
+    if body.isdigit():
+        # Determine amount of calories left in their diet for the day
+        consumedCalories = int(body)   
+        caloriesRemaining = 2000 - consumedCalories
+        resp.message("You have {} Calories remaining for the day (2000 Calorie diet).".format(caloriesRemaining))
+    else:
+        resp.message("Please send Calorie intake only.")
 
     return str(resp)
 
