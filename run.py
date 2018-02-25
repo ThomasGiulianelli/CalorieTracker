7f3e0996a2c38090e8208194e20d13f71f228bb1
ThomasGiulianelli <giuliat3@tcnj.edu>
2018-02-25 14:02:32 -0500

    added dictionary to memorize previously input calories

--- run.py
+++ run.py
@@ -5,11 +5,16 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+#dictionary to store unique phone numbers and their calorie consumption
+users = {}
+
 @app.route("/sms", methods=['GET', 'POST'])
-def calory_reply():
+def calorie_reply():
     """Respond to incoming messages with calorie information SMS."""
     # Get message sent by the user
     body = request.values.get('Body', None)
+    # Get user's phone number
+    user = request.values.get('From', None)
     
     # Start our response
     resp = MessagingResponse()
@@ -17,8 +22,16 @@ def calory_reply():
     # Ensure correct input (digits only)
     if body.isdigit():
         # Determine amount of calories left in their diet for the day
-        consumedCalories = int(body)   
-        caloriesRemaining = 2000 - consumedCalories
+        consumedCalories = int(body) 
+        
+        #if user is already known, add onto their calorie total
+        if user in users:
+            users[user] = users[user] + consumedCalories
+        else:
+            #add user and their calorie consumption to the dictionary
+            users[user] = consumedCalories
+          
+        caloriesRemaining = 2000 - users[user]
         if caloriesRemaining > 0:
             resp.message("You have {} Calories remaining for the day (2000 Calorie diet).".format(caloriesRemaining))
         else:
